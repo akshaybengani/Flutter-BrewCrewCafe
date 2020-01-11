c2ee5c7d5328dadde65eda65bde8d910240a02a3
Akshay Bengani <akshaybengani@gmail.com>
2020-01-12 00:10:32 +0530

    Project Base created

---
+++ android/app/src/main/kotlin/com/example/brew_crew_cafe/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.brew_crew_cafe
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
